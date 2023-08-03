095a1ac021962930caee7f5a85b3dd831629a57b
madroid <madroidmaq@gmail.com>
2023-08-04 00:22:04 +0800

    init project

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+    application
+}
+
+group = "com.madroid.aigc"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+application {
+    mainClass.set("MainKt")
+}
